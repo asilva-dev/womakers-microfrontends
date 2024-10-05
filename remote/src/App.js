64cdbe16edc22e36d895b7533ccfd330337b6d7d
asilva-dev <amanda.adgti@gmail.com>
2024-10-04 22:23:39 -0300

    Alter CSS

--- remote/src/App.js
+++ remote/src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export const App = () => {
   return (
-      <h1 style={{
+      <h1 
+      style={{
         display: "flex",           
         justifyContent: "center",  
-        alignItems: "center",     
-        minHeight: "100vh",      
-        minHeight: "100vh",        
-      }}>
+        alignItems: "center",       
+      }}
+      >
         Aplicação Host!!!
       </h1>  
   ) 
